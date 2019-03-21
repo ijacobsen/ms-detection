c8e38a83eaabe788100a3bcd43a3e0b4904fab1e
ijacobsen <ian.jacobsen.92@gmail.com>
2019-03-21 19:13:37 -0400

    defined network from paper... does not compile yet... dimension size problem

--- scripts/3d_cnn.py
+++ scripts/3d_cnn.py
@@ -8,12 +8,11 @@ import plotly.graph_objs as go
 from matplotlib.pyplot import cm
 import numpy as np
 import keras
-import h5py
 
 import data_handler as dh
 import os
 
-patch_size = (13, 13, 9)
+patch_size = (11, 11, 11)
 
 # get list of available directories
 dir_list = os.listdir('../raw_data/')
@@ -51,34 +50,53 @@ ytrain = np.array(ytrain)
 # convert target variable into one-hot
 y_train = keras.utils.to_categorical(ytrain, 2)
 
+'''
+layer    type    input size          maps    size    stride     pad
+0        input   c x 11 x 11 x 11    -        -       -         -
+1        conv    c x 11 x 11 x 11    32       3^3     1^3       1^3
+2        mp      32 x 5 x 5 x 5      -        2^3     2^3       0
+3        conv    64 x 5 x 5 x 5      64       3^3     1^3       1^3
+4        mp      64 x 2 x 2 x 2      -        2^3     2^3       0
+5        fc      256                 256      1       -         -
+6        soft    2                   2        1       -         -
+'''
+
+
 # input layer
-input_layer = Input(patch_size)
+input_layer = Input(np.hstack((1, patch_size)))
 
-## convolutional layers
-conv_layer1 = Conv3D(filters=8, kernel_size=(3, 3, 3), activation='relu')(input_layer)
-conv_layer2 = Conv3D(filters=16, kernel_size=(3, 3, 3), activation='relu')(conv_layer1)
+# FAILS HERE ^^^
 
-## add max pooling to obtain the most imformatic features
-pooling_layer1 = MaxPool3D(pool_size=(2, 2, 2))(conv_layer2)
+# convolutional layer
+conv_layer1 = Conv3D(filters=32, kernel_size=(3, 3, 3), 
+                     strides=(1, 1, 1), padding='valid', 
+                     activation='relu')(input_layer)
 
-conv_layer3 = Conv3D(filters=32, kernel_size=(3, 3, 3), activation='relu')(pooling_layer1)
-conv_layer4 = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu')(conv_layer3)
-pooling_layer2 = MaxPool3D(pool_size=(2, 2, 2))(conv_layer4)
+# max pooling layer
+pooling_layer1 = MaxPool3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(conv_layer1)
 
-## perform batch normalization on the convolution outputs before feeding it to MLP architecture
+# convolutional layer
+conv_layer2 = Conv3D(filters=64, kernel_size=(3, 3, 3),
+                     strides=(1, 1, 1), padding='valid', 
+                     activation='relu')(pooling_layer1)
+
+# max pooling layer
+pooling_layer2 = MaxPool3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(conv_layer2)
+
+
+# ** UNSURE ABOUT THIS ... copying it from example
+# perform batch normalization on the convolution outputs before feeding it to MLP architecture
 pooling_layer2 = BatchNormalization()(pooling_layer2)
 flatten_layer = Flatten()(pooling_layer2)
 
-## create an MLP architecture with dense layers : 4096 -> 512 -> 10
-## add dropouts to avoid overfitting / perform regularization
-dense_layer1 = Dense(units=2048, activation='relu')(flatten_layer)
-dense_layer1 = Dropout(0.4)(dense_layer1)
-dense_layer2 = Dense(units=512, activation='relu')(dense_layer1)
-dense_layer2 = Dropout(0.4)(dense_layer2)
-output_layer = Dense(units=10, activation='softmax')(dense_layer2)
+
+dense_layer1 = Dense(units=256, activation='relu')(flatten_layer)
+#dense_layer1 = Dropout(0.4)(dense_layer1)
+
+output_layer = Dense(units=2, activation='softmax')(dense_layer1)
 
 ## define the model with input layer and output layer
 model = Model(inputs=input_layer, outputs=output_layer)
 
 model.compile(loss=categorical_crossentropy, optimizer=Adadelta(lr=0.1), metrics=['acc'])
-model.fit(x=xtrain, y=y_train, batch_size=128, epochs=50, validation_split=0.2)
+#model.fit(x=xtrain, y=y_train, batch_size=128, epochs=50, validation_split=0.2)
