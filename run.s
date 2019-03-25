1cc0517b6395a336721933136eab7e2ce68fa798
ijacobsen <ianjacobsen92@gmail.com>
2019-03-25 17:59:43 -0400

    keras 3d CNN running on HPC

--- run.s
+++ run.s
@@ -14,13 +14,11 @@ module purge
 
 module load python/intel/2.7.12
 module load pandas/intel/py2.7/0.20.3 
-module load nibabel/2.1.0
-module load keras/2.0.2
 module load scikit-learn/intel/0.18.1
 module load numpy/python2.7/intel/1.14.0
 
 source ~/ms_det/py2_ms/bin/activate
 
-cd ~/scratch/ij405/test
-python ~/ms_det/3d_cnn.py
+cd ~/ms_det/scripts/
+python ~/ms_det/scripts/3d_cnn.py
 
