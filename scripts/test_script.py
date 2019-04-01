3d74aac8f9eb922787a26f0db03f20a01bc33b9f
ijacobsen <ian.jacobsen.92@gmail.com>
2019-03-31 21:06:55 -0400

    added leave one out training to test script... will save 15 CNNs

--- scripts/test_script.py
+++ scripts/test_script.py
@@ -1,3 +1,7 @@
+'''
+this script will train 15 CNNs, using leave one out
+'''
+
 import keras
 import numpy as np
 import model_lib as ml
@@ -18,57 +22,62 @@ print('data loaded')
 
 # choose a patient
 patient_list = df.index
-patient = patient_list[0]
 
-# get patches
-ex = dh.patcher(patch_size=patch_size)
-ex.patchify(path_table=df, patient=patient)
-patches = ex.patches_xyz
+for i in range(len(patient_list)):
 
-# TODO SHUFFLE PATCHES
+    pats_lv1out = [patient_list[j] for j in range(len(patient_list)) if j != i]
 
-# stack example patches to feed into NN
-x_train = [ptch.array for ptch in patches]
-xtrain = np.ndarray((len(x_train),
-                     x_train[0].shape[0],
-                     x_train[0].shape[1],
-                     x_train[0].shape[2],
-                     num_channels))
-ytrain = [int(ptch.label) for ptch in patches]
+    first_iteration = True
+    for patient in pats_lv1out:
+    
+        # get patches
+        ex = dh.patcher(patch_size=patch_size)
+        ex.patchify(path_table=df, patient=patient)
+        patches = ex.patches_xyz
+    
+        # TODO SHUFFLE PATCHES
+    
+        # stack example patches to feed into NN
+        x_train = [ptch.array for ptch in patches]
+        xtrain = np.ndarray((len(x_train),
+                             x_train[0].shape[0],
+                             x_train[0].shape[1],
+                             x_train[0].shape[2],
+                             num_channels))
+        ytrain = [int(ptch.label) for ptch in patches]
+    
+        # TODO this is very sloppy... xtrain[i, :, :, :, 0] ... 0 is hardcoded in
+        # to account for only 1 channel (modality) being used... fix this!
+        # fill xdata with patch data
+        for i in range(len(xtrain)):
+            xtrain[i, :, :, :, 0] = x_train[i]
+        ytrain = np.array(ytrain)
+    
+        # convert target variable into one-hot
+        y_train = keras.utils.to_categorical(ytrain, 2)
+    
+        # before stacking, 
+        # xtrain (500, 11, 11, 11, 1)
+        # y_train (500, 2)
+    
+        if first_iteration:
+            xtrain_all = xtrain
+            ytrain_all = y_train
+            first_iteration = False
+        else:
+            xtrain_all = np.concatenate((xtrain_all, xtrain), 0)
+            ytrain_all = np.concatenate((ytrain_all, y_train), 0)
 
-# TODO this is very sloppy... xtrain[i, :, :, :, 0] ... 0 is hardcoded in
-# to account for only 1 channel (modality) being used... fix this!
-# fill xdata with patch data
-for i in range(len(xtrain)):
-    xtrain[i, :, :, :, 0] = x_train[i]
-ytrain = np.array(ytrain)
-
-# convert target variable into one-hot
-y_train = keras.utils.to_categorical(ytrain, 2)
-
-# before stacking, 
-# xtrain (500, 11, 11, 11, 1)
-# y_train (500, 2)
-
-'''    
-    if first_iteration:
-        xtrain_all = xtrain
-        y_train_all = y_train
-        first_iteration = False
-    else:
-        xtrain_all = np.concatenate((xtrain_all, xtrain), 0)
-        y_train_all = np.concatenate((y_train_all, y_train), 0)
-'''
-
-#%% CNN STUFF
-
-# initiate model
-model_name = 'test_model'
-#model = ml.cnn_model(name=model_name, mode='train')
-
-# train model
-#model.train_network(xtrain=xtrain, ytrain=y_train, batch_size=8, epochs=50)
-
-# load model
-model = ml.cnn_model(name=model_name, mode='load')
+    ### CNN STUFF
+    
+    # initiate model
+    model_name = 'lv1out_{}'.format(patient_list[i])
+    model = ml.cnn_model(name=model_name, mode='train')
+    
+    # train model
+    model.train_network(xtrain=xtrain_all, ytrain=ytrain_all, 
+                        batch_size=8, epochs=50)
+    
+    # load model
+    # model = ml.cnn_model(name=model_name, mode='load')
 
