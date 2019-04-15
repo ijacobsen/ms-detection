eeec166c08ed1c4eee28d80cd964703c94046b8c
ijacobsen <ian.jacobsen.92@gmail.com>
2019-04-15 19:36:26 -0400

    getting evaluation to work... not done yet

--- scripts/example_test_script.py
+++ scripts/example_test_script.py
@@ -23,16 +23,30 @@ patches = ex.patches_xyz
 
  #%% IDK... I DIDNT DO THIS YET
 # stack example patches to feed into NN
+'''
 x_train = [ptch.array for ptch in patches]
 xtrain = np.ndarray((len(x_train),
                      x_train[0].shape[0],
                      x_train[0].shape[1],
                      x_train[0].shape[2],
                      num_channels))
+'''
 
 #%% load network
+# the network tests patches in xyz format... more specifically,
+# (batch_size, x, y, z, 1), where 1 is because there is only 1 modality
 mdl_dir = 'trained_models_layer1'
 model = ml.cnn_model(mode='load', name=patient, path=mdl_dir)
+
+
+batch_sz = 1
+form = np.ndarray((batch_sz, 
+                   patches[0].array.shape[0],
+                   patches[0].array.shape[1],
+                   patches[0].array.shape[2],
+                   1))
+form[0, :, :, :, 0] = patches[0].array
+prediction = model.model.predict(form)
 
 #%% evaluate patient
 # for each pixel, create a patch and feed into network
