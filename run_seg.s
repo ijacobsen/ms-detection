6f5c02b632ab4dfd07a3f396d51d8a1d1ac295e3
ijacobsen <ianjacobsen92@gmail.com>
2019-05-10 19:59:09 -0400

    running on GPU

--- run_seg.s
+++ run_seg.s
@@ -3,21 +3,19 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
-#SBATCH --time=8:00:00
-#SBATCH --mem=64GB
-#SBATCH --job-name=777
+#SBATCH --time=5:00:00
+#SBATCH --mem=32GB
+#SBATCH --job-name=segment
 #SBATCH --mail-type=END
 #SBATCH --mail-user=ij405@nyu.edu
 #SBATCH --output=slurm_%j.out
 
 module purge
 
-module load cuda/9.0.176 cudnn/9.0v7.0.5
-
-source activate py2_gpu
+source activate py2
 
 cd ~/ms_det/scripts/
 #python ~/ms_det/trouble/test_slurm.py
 #python ~/ms_det/scripts/trouble.py
-python ~/ms_det/scripts/full_script.py
+python ~/ms_det/scripts/segment.py
 #python ~/ms_det/trouble/eval_perf.py
--- scripts/full_script.py
+++ scripts/full_script.py
@@ -33,7 +33,7 @@ import os
 patch_size = (11, 11, 11) #(x, y, z)
 num_channels = 1
 batch_sz = 64
-epochs_hp = 15
+epochs_hp = 30
 num_pats = 5
 
 # get list of available directories
