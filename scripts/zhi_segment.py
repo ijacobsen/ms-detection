ed819bf9503172fe094dda278aaf5f262742635a
ijacobsen <ianjacobsen92@gmail.com>
2019-05-16 21:09:32 -0400

    running zhi segmentations on GPU

--- scripts/zhi_segment.py
+++ scripts/zhi_segment.py
@@ -5,10 +5,10 @@ import numpy as np
 import os
 
 # select hyperparameters
-n1lr = '0.003'
+n1lr = '0.03'
 n2lr = '0.003'
-btchsz = 32
-epochs = 1000
+btchsz = 128
+epochs = 240
 
 # get list of available directories
 dir_list = os.listdir('../raw_data/')
@@ -30,19 +30,20 @@ mdl_dir = '/scratch/ij405/zhi_models'
 # parameters
 n1params = 'lr=' + n1lr + 'btch=' + str(btchsz)
 n2params = 'lr=' + n2lr + 'btch=' + str(btchsz)
+params = 'n1lr=' + n1lr + '_n2lr=' + n2lr + '_btch=' + str(btchsz) 
 
 # segment image
 for patient in test_pats:
     
     # classify FLAIR image
     classifier = ml.classifier(mode='classify',
-                               n1name=n1params, n2name=n2params,
+                               n1name=params, n2name=params,
                                path=mdl_dir, data=df, zhi=True)
     [n1, n2] = classifier.classify_scan(patient=patient)
     
     # save classified pixels to file
-    np.save('{}_seg_'.format(patient), [n1, n2])
-    np.savez_compressed('{}_segmentations_compressed'.format(patient), [n1, n2])
+    np.save('{}_seg_{}'.format(patient, params), [n1, n2])
+    np.savez_compressed('{}_seg_compressed_{}'.format(patient, params), [n1, n2])
 
 # to load
 #[n1, n2] = np.load('{}_segmentations.npy'.format(patient))
